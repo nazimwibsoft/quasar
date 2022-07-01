13697b448adc4284a6f442a9714d40e00d56d373
Muhammad Nazim <muhammad.nazim.wibsoft@gmail.com>
2022-07-01 08:52:49 +0500

    Sample code recaptcha

---
+++ quasar2/recaptcha/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+  <VueBrowserUpdate />
+</template>
+<script>
+import { defineComponent } from 'vue'
+import VueBrowserUpdate from 'vue-browserupdate'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    VueBrowserUpdate
+  }
+})
+</script>
---
+++ quasar2/recaptcha/src/pages/Recaptcha.vue
@@ -0,0 +1,24 @@
+<template>
+  <vue-recaptcha sitekey="6LcmbbgfAAAAAC55miOZbaWqBl8U-ppWIJBvC5L1" @verify="onVerify">
+  </vue-recaptcha>
+
+  <q-btn :disabled=!verified color="primary" label="Next" />
+</template>
+
+<script>
+import { VueRecaptcha } from 'vue-recaptcha'
+export default {
+  data () {
+    return {
+      verified: false
+    }
+  },
+  components: { VueRecaptcha },
+  methods: {
+    onVerify () {
+      this.verified = true
+      console.log('Verified')
+    }
+  }
+}
+</script>
